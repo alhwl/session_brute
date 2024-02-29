ca5304c4b29f92296142f7f601d24a797516b5da
alhwl <143041398+alhwl@users.noreply.github.com>
2024-02-29 12:50:33 -0600

    Update pdfbrut.py

--- pdfbrut.py
+++ pdfbrut.py
@@ -1,1 +1,25 @@
+import subprocess
 
+# Path to the word list file
+file_path = 'rockyou.txt'
+
+# Command template
+base_command = 'pdftk encrypted.pdf input_pw {} output output.pdf'
+
+# Open the file and read each line
+with open(file_path, 'r', encoding='latin-1') as file:
+    for line in file:
+        # Strip newline characters from each line
+        password = line.strip()
+        
+        # Substitute {} with the current password
+        command_to_run = base_command.format(password)
+        
+        try:
+            # Execute the command
+            subprocess.run(command_to_run, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+            print(f"Success with password: {password}")
+            break  # If successful, stop the loop
+        except subprocess.CalledProcessError:
+            # If the command failed (wrong password), continue with the next one
+            continue
